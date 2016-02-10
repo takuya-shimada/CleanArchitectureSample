c6c34bf1951a1133bd015312c366b1ec313f3408
Takuya Shimada <shimada@1923.co.jp>
2016-02-10 12:59:04 +0900

    add FindArticlesTest

--- app/src/androidTest/java/tyfrontier/cleanarchitecturesample/di/component/TestAppComponent.java
+++ app/src/androidTest/java/tyfrontier/cleanarchitecturesample/di/component/TestAppComponent.java
@@ -5,15 +5,19 @@ import javax.inject.Singleton;
 import dagger.Component;
 import tyfrontier.cleanarchitecturesample.di.module.TestAppModule;
 import tyfrontier.cleanarchitecturesample.di.module.TestDataModule;
+import tyfrontier.cleanarchitecturesample.di.module.TestDomainModule;
 import tyfrontier.cleanarchitecturesample.domain.net.WebApiTest;
+import tyfrontier.cleanarchitecturesample.domain.usecase.FindArticlesTest;
 
 @Component(
         modules = {
                 TestAppModule.class,
+                TestDomainModule.class,
                 TestDataModule.class,
         }
 )
 @Singleton
 public interface TestAppComponent extends AppComponent {
     void inject(WebApiTest webApiTest);
+    void inject(FindArticlesTest findArticlesTest);
 }
