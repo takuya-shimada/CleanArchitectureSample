7672d77156abbd2d56eb2360300c68a506de3862
Takuya Shimada <shimada@1923.co.jp>
2016-02-10 12:03:58 +0900

    add test runner
    add

---
+++ app/src/androidTest/java/tyfrontier/cleanarchitecturesample/TestMainApplication.java
@@ -0,0 +1,9 @@
+package tyfrontier.cleanarchitecturesample;
+
+public class TestMainApplication extends MainApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
