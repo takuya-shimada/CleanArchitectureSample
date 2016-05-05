8239ebcde72c727cab031ac5c9ffb1d5800170e1
Takuya Shimada <ty.frontier@gmail.com>
2016-05-05 19:26:26 +0900

    cache articles

--- app/src/main/java/tyfrontier/cleanarchitecturesample/data/repository/ArticleRepositoryImpl.java
+++ app/src/main/java/tyfrontier/cleanarchitecturesample/data/repository/ArticleRepositoryImpl.java
@@ -1,5 +1,8 @@
 package tyfrontier.cleanarchitecturesample.data.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import rx.Observable;
@@ -10,6 +13,7 @@ import tyfrontier.cleanarchitecturesample.domain.repository.ArticleRepository;
 public class ArticleRepositoryImpl implements ArticleRepository {
 
     private final WebApi webApi;
+    private final List<Article> cache = new ArrayList<>();
 
     @Inject
     public ArticleRepositoryImpl(WebApi webApi) {
@@ -18,6 +22,6 @@ public class ArticleRepositoryImpl implements ArticleRepository {
 
     @Override
     public Observable<Article> findArticles() {
-        return webApi.getArticles(1, 20);
+        return cache.isEmpty() ? webApi.getArticles(1, 20).doOnNext(cache::add) : Observable.from(cache);
     }
 }
